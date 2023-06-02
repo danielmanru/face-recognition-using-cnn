84b215cc728f66c9649c122bc311e745da311aee
danielmanru <85722906+danielmanru@users.noreply.github.com>
2023-06-02 09:22:37 +0700

    update realtime recognition

--- index.py
+++ index.py
@@ -1,18 +1,15 @@
 # import pandas as pd
 import cv2
 import streamlit as st
-# from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
 from PIL import Image
 import numpy as np
 import tensorflow as tf
 import keras
 from keras.utils import load_img, img_to_array
-# from keras.preprocessing import image
 from random import randrange
 from keras.preprocessing.image import ImageDataGenerator
 import pickle
 from keras.models import load_model
-# import face_recognition
 loaded_model = load_model('face_model.h5')
 
 train_dir = "sample_data/Original_Images/"
@@ -22,8 +19,8 @@ train_ds = generator.flow_from_directory(
 classes = list(train_ds.class_indices.keys())
 
 cap = cv2.VideoCapture(0)
-FRAME_WINDOW = st.image([])
-text = st.empty()
+
+faceDeteksi = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 def predict_image(images_pred, file_name):
@@ -44,7 +41,8 @@ def realtime_predict_image(realtime_images_pred):
     images = np.vstack([x])
     pred = loaded_model.predict(images)
     predicted = classes[np.argmax(pred)]
-    return predicted
+    result = "Predict : " + predicted
+    return result
 
 
 def main():
@@ -69,13 +67,19 @@ def main():
     if choice == 'Realtime':
         st.subheader("Realtime Face Recognition")
         run = st.checkbox("Run camera")  # checkbox
+        FRAME_WINDOW = st.image([])
+        text = st.empty()
         if run == True:
             while True:
                 success, img = cap.read()
-                imgS = cv2.resize(img, (224, 224))
-                imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+                imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                wajah = faceDeteksi.detectMultiScale(imgS, 1.3, 5)
+                for (x, y, w, h) in wajah:
+                    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
                 FRAME_WINDOW.image(img)
-                text.write(realtime_predict_image(imgS))
+                img_source = cv2.resize(imgS, (224, 224))
+                text.write(realtime_predict_image(img_source))
                 cv2.waitKey(1)
         else:
             pass
